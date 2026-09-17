e6b7e3a738afbcd0b4c362c3b1868158d1750192
Dan <danielmillst@gmail.com>
2026-09-17 07:05:02 -0500

    fix: expose string-view literals for generated SKSE metadata

--- src/PCH.h
+++ src/PCH.h
@@ -2,3 +2,5 @@
 #include <RE/Skyrim.h>
 #include <SKSE/SKSE.h>
 #include <spdlog/spdlog.h>
+// CommonLib's generated plugin metadata uses string_view literal suffixes.
+using namespace std::literals;
